19a780cdd421cb49bd885e358a3da288a4bcf4f5
Brian Valerio <brivalerio@gmail.com>
2019-06-29 22:35:30 -0700

    Saving progress

--- Climate_App.py
+++ Climate_App.py
@@ -21,3 +21,36 @@ Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+
+#  Flask
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return(
+        f"PLEASE READ: Paste the routes after the default browser link (Example: http://127.0.0.1:5000/api/v1.0/tobs)</br>"
+        f"</br>"
+        f"***AVAILABLE ROUTES***</br>"
+        f"</br>"
+        f"/api/v1.0/precipitation</br>"       
+        f"--Show precipitation for all days in entire dataset</br>"
+        f"</br>"
+        f"/api/v1.0/stations</br>"       
+        f"--Show listing of all stations in dataset</br>"
+        f"</br>"
+        f"/api/v1.0/tobs</br>"
+        f"--Shows temperature observations a year from the last data point</br>"
+        f"</br>"
+        f"/api/v1.0/'START-DATE-HERE'</br>"
+        f"--Start date only: will calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date</br>"
+        f"--Note: please input date as shown: '2016-04-01'</br>"
+        f"--Example link: http://127.0.0.1:5000/api/v1.0/'2016-04-01'</br>"
+        f"</br>"
+        f"/api/v1.0/'START-DATE-HERE'/'END-DATE-HERE'</br>"
+        f"--Start and end date: calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive</br>"
+        f"--Note: please input dates as shown: '2016-04-01'</br>"
+        f"--Example link: http://127.0.0.1:5000/api/v1.0/'2016-04-01'/'2016-04-13'</br>"
+        )
+
+if __name__ == '__main__':
+    app.run(debug=True)
